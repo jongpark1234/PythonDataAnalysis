8e054af74d4bf286f156dd22ac195268118fc81e
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-07-31 02:47:13 +0900

    Project

--- ICT/asd.py
+++ ICT/asd.py
@@ -93,7 +93,7 @@ plt.rc('font', family='NanumBarunGothic')
 ratio = [34, 32, 15, 18]
 labels = ['Apple', 'Banana', 'Melon', 'Grapes']
 explode = [0, 0.1, 0, 0]
-colors = ['silver, gold, whitesmoke', 'lightgray']
+colors = ['silver', 'gold', 'whitesmoke', 'lightgray']
 
 plt.pie(ratio, labels=labels, autopct='%.1f%%', explode=explode, shadow=True, colors=colors)
 plt.show()
---
+++ ICT/project1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rc('font', family='NanumBarunGothic')
+
+# data = [2720067, 455159, 415904, 556633, 513993, 558315, 786681, 838689, 149889]
+# label = ['경기도', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주특별자치도']
+
+# plt.title('노인 인구분포 현황', size=20)
+# plt.pie(data, labels=label, explode=[0.1 for _ in range(9)], autopct='%.1f%%', textprops={'fontsize': 12})
+# plt.show()
+
+# plt.ylim(25, 40)
+# plt.xlabel('연도')
+# plt.ylabel('취업률')
+# plt.title('노인 취업 현황', size = 20)
+# plt.plot(['2011', '2014', '2017', '2020'], [34.0, 28.9, 30.9, 36.9], 'o-', color='orange')
+# plt.grid()
+# plt.show()
+
